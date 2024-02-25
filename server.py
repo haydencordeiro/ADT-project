6410f7b7b80e808b8df592be8d612858aaf603cf
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-02-24 21:30:42 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,37 @@
+import joblib
+loaded_model = joblib.load('gradient_boosting_model.pkl')
+
+# print(loaded_model.predict([[2.01000000e+03,1020662522, 9.21147059e+00, 1.79620294e+01,
+#        7.02976471e+00, 7.98700000e+01, 1.93850000e+02, 3.65500000e+02]]))
+
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Define the route for the homepage
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get the input values from the form
+        year = int(request.form['year'])
+        population = float(request.form['population'])
+        unemployment_above_15 = float(request.form['unemployment_above_15'])
+        unemployment_between_15_24 = float(request.form['unemployment_between_15_24'])
+        unemployment_above_25 = float(request.form['unemployment_above_25'])
+        share_of_agricultural_land = float(request.form['share_of_agricultural_land'])
+        people_employed_in_agriculture = float(request.form['people_employed_in_agriculture'])
+        total_employment_in_africa = float(request.form['total_employment_in_africa'])
+        # Make prediction using your model
+        prediction = loaded_model.predict([[year, population, unemployment_above_15, unemployment_between_15_24, unemployment_above_25, share_of_agricultural_land, people_employed_in_agriculture, total_employment_in_africa]])
+        print([[year, population, unemployment_above_15, unemployment_between_15_24, unemployment_above_25, share_of_agricultural_land, people_employed_in_agriculture, total_employment_in_africa]])
+        print(prediction)
+        
+        # Return the predicted value as JSON
+        return jsonify({'prediction': prediction[0]})
+    
+    # If it's a GET request, render the HTML template
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
